90517132d518ddd9d1acbb50254d200c9849fb70
kdog0625 <kdoooog34@gmail.com>
2022-05-01 07:55:28 +0900

    つぶやきを投稿する処理を追記

---
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Tweet\CreateRequest;
+use App\Models\Tweet;
+use Illuminate\Http\RedirectResponse;
+
+
+class CreateController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param CreateRequest $request
+     * @return RedirectResponse
+     */
+    public function __invoke(CreateRequest $request): RedirectResponse
+    {
+        $tweet = new Tweet;
+        $tweet->content = $request->tweet();
+        $tweet->save();
+        return redirect()->route('tweet.index');
+    }
+}
