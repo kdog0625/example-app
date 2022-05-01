26a099aa78c508dc6d0446216c1b03c3309496ae
kdog0625 <kdoooog34@gmail.com>
2022-05-02 00:07:55 +0900

    ログイン機能の追加

--- app/Http/Controllers/Tweet/Update/IndexController.php
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers\Tweet\Update;
 
 use App\Http\Controllers\Controller;
 use App\Models\Tweet;
+use App\Services\TweetService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IndexController extends Controller
@@ -13,11 +18,16 @@ class IndexController extends Controller
      * Handle the incoming request.
      *
      * @param Request $request
-     * @return void
+     * @param TweetService $tweetService
+     * @return Application|Factory|View
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         $tweetId = (int) $request->route('tweetId');
+        if (!$tweetService->checkOwnTweet($request->user()->id, $tweetId)) {
+            throw new AccessDeniedHttpException();
+        }
+
         $tweet = Tweet::where('id', $tweetId)->firstOrFail();
         return view('tweet.update')->with('tweet', $tweet);
     }
